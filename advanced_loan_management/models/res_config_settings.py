d4f9141148ac14ac34c790fb8cc387d460e331c2
bmazariegos <bryanestuard3@gmail.com>
2025-10-27 09:12:45 -0600

    [FIX] Se cambios por falta de verificación multiempresa de configuración

--- advanced_loan_management/models/res_config_settings.py
+++ advanced_loan_management/models/res_config_settings.py
@@ -10,24 +10,24 @@ class ResConfigSettings(models.TransientModel):
     interest_product_id = fields.Many2one('product.product',
                                           string="Interest Product",
                                           config_parameter="advanced_loan_management.interest_product_id",
-                                          company_dependent=True,
+                                          check_company=True,
                                           help="Product For Interest "
                                                "To Create Invoice Lines")
     repayment_product_id = fields.Many2one('product.product',
                                            string="Repayment Product",
                                            config_parameter="advanced_loan_management.repayment_product_id",
-                                           company_dependent=True,
+                                           check_company=True,
                                            help="Product For Repayment "
                                                 "To Create Invoice Lines")
     disbursement_account_id = fields.Many2one('account.account',
                                            string="Disbursement account",
                                            config_parameter="advanced_loan_management.disbursement_account_id",
                                            domain="[('account_type', '=', 'liability_current'),('company_id', '=', company_id)]",
-                                           company_dependent=True,
+                                           check_company=True,
                                            help="Loan management disburse accounts")
     repayment_account_id = fields.Many2one('account.account',
                                            string="Repayment accounts",
                                            config_parameter="advanced_loan_management.repayment_account_id",
                                            domain="[('account_type', '=', 'asset_cash'),('company_id', '=', company_id)]",
-                                           company_dependent=True,
+                                           check_company=True,
                                            help="Loan payment account")
