97d99137d5627825e389211b589bf4df1b779025
bmazariegos <bryanestuard3@gmail.com>
2025-11-06 12:02:44 -0600

    [FIX] Se modifica la versión de módulo de préstamos

--- advanced_loan_management/__manifest__.py
+++ advanced_loan_management/__manifest__.py
@@ -16,7 +16,7 @@
     'website': 'https://www.hw.com.gt',
     
     'category': 'Accounting',
-    'version': '17.0.1.0.4',
+    'version': '17.0.2.1',
     
     'depends': ['base',
                 'mail', 
