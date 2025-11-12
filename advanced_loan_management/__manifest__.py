519ddc61dfcf6617948d2558c1f0e137dd6fdba2
bmazariegos <bryanestuard3@gmail.com>
2025-11-12 14:59:13 -0600

    [FEAT] Se agrega traducciones.

--- advanced_loan_management/__manifest__.py
+++ advanced_loan_management/__manifest__.py
@@ -16,7 +16,7 @@
     'website': 'https://www.hw.com.gt',
     
     'category': 'Accounting',
-    'version': '17.0.2.1',
+    'version': '17.0.3.1',
     
     'depends': ['base',
                 'mail', 
