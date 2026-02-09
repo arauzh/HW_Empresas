f7cdbfa62f3aaa0083fc30935836573fc1e7f837
Josea2024 <josea20241@gmail.com>
2026-02-09 08:42:14 -0600

    FIX Libro Mayor

--- custom_general_ledger/reports/report_general_ledger_parser.py
+++ custom_general_ledger/reports/report_general_ledger_parser.py
@@ -13,10 +13,14 @@ class ReportCustomLedger(models.AbstractModel):
             
         # 1. Consulta Saldo Inicial
         query_initial = """
-            SELECT aml.account_id, SUM(aml.debit) - SUM(aml.credit) as initial_balance
+            SELECT
+                aml.account_id,
+                SUM(aml.debit - aml.credit) AS initial_balance
             FROM account_move_line aml
-            JOIN account_move am ON am.id = aml.move_id
-            WHERE am.date < %s AND am.company_id = %s AND am.state = ANY(%s)
+            WHERE aml.date < %s
+            AND aml.company_id = %s
+            AND aml.parent_state = ANY(%s)
+            AND aml.display_type IN ('product', 'tax', 'payment_term')
             GROUP BY aml.account_id
         """
         cr.execute(query_initial, (date_from, company_id, move_state))
@@ -24,10 +28,16 @@ class ReportCustomLedger(models.AbstractModel):
 
         # 2. Consulta Movimientos del Periodo
         query_period = """
-            SELECT aml.account_id, SUM(aml.debit) as debit, SUM(aml.credit) as credit
+            SELECT
+                aml.account_id,
+                SUM(aml.debit) AS debit,
+                SUM(aml.credit) AS credit
             FROM account_move_line aml
-            JOIN account_move am ON am.id = aml.move_id
-            WHERE am.date >= %s AND am.date <= %s AND am.company_id = %s AND am.state = ANY(%s)
+            WHERE aml.date >= %s
+            AND aml.date <= %s
+            AND aml.company_id = %s
+            AND aml.parent_state = ANY(%s)
+            AND aml.display_type IN ('product', 'tax', 'payment_term')
             GROUP BY aml.account_id
         """
         cr.execute(query_period, (date_from, date_to, company_id, move_state))
