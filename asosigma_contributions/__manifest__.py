e869fa8d118fdb0493ab291592134bb06f496722
Jose Ramirez <josea20241@gmail.com>
2026-08-03 09:03:01 -0600

    Rebuild

--- asosigma_contributions/__manifest__.py
+++ asosigma_contributions/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'ASOSIGMA Contributions',
-    'version': '17.0.0.1',
+    'version': '17.0.0.2',
     'category': 'Accounting',
     'summary': 'Gestión de aportaciones y ahorros ASOSIGMA',
     "author": "HW Contractor",
